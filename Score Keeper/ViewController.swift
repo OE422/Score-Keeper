77db3a1f01a71a71170fd62084bb5e49527a073b
OE422 <omareldoronki@gmail.com>
2019-09-19 14:08:39 -0400

    make uitextfields work

--- Score Keeper/ViewController.swift
+++ Score Keeper/ViewController.swift
@@ -8,37 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     var scoreLabel = UILabel()
     var scoreLabel2 = UILabel()
     var challenge = UILabel()
+    var nameField = UITextField()
+    var nameField2 = UITextField()
     var pointIncrease = UIButton()
     var pointDecrease = UIButton()
     var pointIncrease2 = UIButton()
     var pointDecrease2 = UIButton()
+    var reset = UIButton()
     var KSJesko = UIImageView()
     var score = 0
     var score2 = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.becomeFirstResponder()
+        nameField.delegate = self
+        nameField2.delegate = self
         
         scoreLabel.font = UIFont.systemFont(ofSize: 16.0, weight: .bold)
         addSub1AndCenter(arg: scoreLabel, arg: score)
+        scoreLabel.center.y = 100
         self.view.addSubview(scoreLabel)
         
         scoreLabel2.font = UIFont.systemFont(ofSize: 16.0, weight: .bold)
         addSub1AndCenter(arg: scoreLabel2, arg: score2)
+        scoreLabel2.center.y = 130
         self.view.addSubview(scoreLabel2)
         
         challenge.textColor = UIColor.black
         challenge.font = UIFont.systemFont(ofSize: 14.0, weight: .bold)
-        challenge.text = "Can you tap faster than the Jesko?"
-        challenge.textAlignment = NSTextAlignment.center
-        challenge.sizeToFit()
-        challenge.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((challenge.frame.maxX-challenge.frame.minX)/2), y: KSJesko.frame.maxY + 60)
         self.view.addSubview(challenge)
-        
         pointIncrease.setTitle("Add 1", for: .normal)
         pointIncrease.sizeToFit()
         pointIncrease.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((pointIncrease.frame.maxX-pointIncrease.frame.minX)/2), y: 600)
@@ -55,6 +58,8 @@ class ViewController: UIViewController {
         pointDecrease.addTarget(self, action: #selector(self.pressed2), for: .touchUpInside)
         self.view.addSubview(pointDecrease)
         
+    
+        
         pointIncrease2.setTitle("Add 1", for: .normal)
         pointIncrease2.sizeToFit()
         pointIncrease2.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((pointIncrease2.frame.maxX-pointIncrease2.frame.minX)/2), y: 725)
@@ -70,6 +75,14 @@ class ViewController: UIViewController {
         pointDecrease2.setTitleColor(UIColor.black, for: .normal)
         pointDecrease2.addTarget(self, action: #selector(self.pressed2v2), for: .touchUpInside)
         self.view.addSubview(pointDecrease2)
+        
+        reset.setTitle("Reset scores", for: .normal)
+        reset.sizeToFit()
+        reset.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((reset.frame.maxX-reset.frame.minX)/2), y: 775)
+        reset.titleLabel?.font = UIFont.systemFont(ofSize: 14.0, weight: .bold)
+        reset.setTitleColor(UIColor.black, for: .normal)
+        reset.addTarget(self, action: #selector(self.resetPress), for: .touchUpInside)
+        self.view.addSubview(reset)
         
         KSJesko.image = UIImage(named: "jesko")
         KSJesko.frame = CGRect(x: 60, y: 400, width: 320, height: 180)
@@ -104,12 +117,19 @@ class ViewController: UIViewController {
             addSub1AndCenter(arg: scoreLabel2, arg: score2)
         }
     }
+    @objc func resetPress (sender: UIButton!)
+    {
+        score = 0
+        score2 = 0
+        addSub1AndCenter(arg: scoreLabel, arg: score)
+        addSub1AndCenter(arg: scoreLabel2, arg: score2)
+    }
     func addSub1AndCenter(arg UILbl: UILabel, arg points: Int)
     {
-        UILbl.text = "Score: \(score)"
+        UILbl.text = "Score: \(points)"
         UILbl.textAlignment = NSTextAlignment.center
         UILbl.sizeToFit()
-        UILbl.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((UILbl.frame.maxX-UILbl.frame.minX)/2), y: 100)
+        UILbl.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((UILbl.frame.maxX-UILbl.frame.minX)/2), y: UILbl.frame.origin.y)
         if points > 0 && points % 7 == 0
         {
             UILbl.textColor = UIColor.yellow
@@ -118,6 +138,28 @@ class ViewController: UIViewController {
         {
             UILbl.textColor = UIColor.black
         }
+        if (score > score2)
+        {
+            challenge.text = "Player 1 is in the lead."
+        }
+        else if (score < score2)
+        {
+            challenge.text = "Player 2 is in the lead."
+        }
+        else if (score == score2 && score != 0 && score2 != 0)
+        {
+            challenge.text = "Player 1 and player 2 are tied."
+        }
+        else
+        {
+            challenge.text = "Can you tap faster than the Jesko?"
+        }
+        challenge.textAlignment = NSTextAlignment.center
+        challenge.sizeToFit()
+        challenge.frame.origin = CGPoint(x: (UIScreen.main.bounds.width/2)-((challenge.frame.maxX-challenge.frame.minX)/2), y: 60)
+        
+        
+        
     }
 
 
